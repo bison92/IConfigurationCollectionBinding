c29534f41cf07ea22107493d45999fe546462406
Pablo Calderón Recalde <pcalderon@plainconcepts.com>
2019-12-10 11:16:32 +0100

    Test project for IConfiguration with clear default settings

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public IOptions<SettingsWithCollection> Options { get; }
+
+        public TestController(IOptions<SettingsWithCollection> options)
+        {
+            
+            Options = options;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Options.Value.props;
+        }
+    }
+}
---
+++ WebApplication1/SettingsWithCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class SettingsWithCollection
+    {
+        public ICollection<string> props { get; set; } = new List<string>() { "default" };
+    }
+}
